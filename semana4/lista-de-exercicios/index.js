b483220f0e52ae429aef5a583aed542ef5c800ca
Ivana Batista Marinho <ivanammarinho@hotmail.com>
2020-03-14 15:31:05 -0300

    Exercicio2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -8,7 +8,17 @@ Como ela faz?
 O prompt pede que o usuário digite o valor da cotação do dolar. A função recebe um valor em dolar como parâmetro e retorna o valor multiplicado pela cotação, ou seja, convertido em reais.
 
 O que será impresso no console?
-
 R$ + o resultado da multiplicação de 100 pela cotação do dolar informada. 
 
+
+2. 
+O que a função faz?
+A função calcula valores de investimentos.
+
+Como ela faz?
+A função recebe dois parâmetros: tipo de investimento e valor. A função vai encaixar o tipo de investimento informado no caso ao qual ele corresponde no switch e multiplicar o valor indicado no parâmetro pelo valor que está no caso correspondente. Se não corresponder a nenhum caso, entra no default.
+
+O que será impresso no console?
+165
+TIPO DE INVESTIMENTO INFORMADO INCORRETO!
 */

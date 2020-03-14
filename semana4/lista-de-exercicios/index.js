f3b3c9b8e7b6407b56dea4c486a8142aca493c9c
Ivana Batista Marinho <ivanammarinho@hotmail.com>
2020-03-14 15:15:21 -0300

    Exercicio1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,14 @@
+/*Exercícios de Interpretação de Código
+
+1.
+O que a função faz?
+A função converte um valor em dólar para real.
+
+Como ela faz?
+O prompt pede que o usuário digite o valor da cotação do dolar. A função recebe um valor em dolar como parâmetro e retorna o valor multiplicado pela cotação, ou seja, convertido em reais.
+
+O que será impresso no console?
+
+R$ + o resultado da multiplicação de 100 pela cotação do dolar informada. 
+
+*/

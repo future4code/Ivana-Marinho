499824b5b3bdd5b25be875053aedc5cba083b59b
Ivana Batista Marinho <ivanammarinho@hotmail.com>
2020-04-01 17:13:35 -0300

    future4users

--- semana7/semana7/future4users/future4users/src/App.js
+++ semana7/semana7/future4users/future4users/src/App.js
@@ -2,23 +2,54 @@ import React from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import Registro from './Components/Registro.js';
+import Lista from './Components/Lista.js';
 
 class App extends React.Component {
   constructor(props){
     super(props)
-
-    this.state={
-      name: "",
-      email: ""
+    this.state = {
+      selectedPage: "Registro"
     }
   }
 
+  onClickButton = () => {
+     let newPage;
 
+     switch(this.state.selectedPage){
+       case "Registro":
+         newPage = "Lista";
+         break;
+        case "Lista":
+          newPage = "Registro";
+          break;
+        default:
+          newPage = "Registro";
+      }
+      this.setState({selectedPage: newPage})
+  }
+
+  getPageToRender = () => {
+    switch(this.state.selectedPage){
+      case "Registro":
+        return <Registro/>
+        
+       case "Lista":
+         return <Lista/>
+       
+       default:
+         return <Registro/>
+     }
+  }
+ 
   render(){
+    const currentPage = this.getPageToRender();
   return (
     <div>
-      <button>Ir para página de lista</button>
-      <Registro name={this.props.name} email={this.props.email}/>
+      <button onClick={this.onClickButton}>
+        {this.state.selectedPage === "Registro"
+          ? "Ir para a próxima lista" : "Voltar"}
+      </button>
+       {currentPage}  
     </div>
   );
 }
--- semana7/semana7/future4users/future4users/src/Components/Registro.js
+++ semana7/semana7/future4users/future4users/src/Components/Registro.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 
+const urlBase = "https://us-central1-future-apis.cloudfunctions.net/api";
+
 const Container = styled.div`
     width: 350px;
     height: 190px;
@@ -14,45 +16,64 @@ const Container = styled.div`
 
 
 class Registro extends React.Component{
-    createUser = () => {
-        const body = {
-            name: this.StaticRange.inputName,
-            email: this.StaticRange.inputEmail
+    constructor(props){
+        super(props)
+
+        this.state = {
+            nameValue: "",
+            emailValue: ""
         }
-    
-       axios.post('https://us-central1-future-apis.cloudfunctions.net/api/users', body,
-    {
-        headers: {
-            'api-token': 'Ivana-Hamilton'
-        }
-    })
-    
-    .then(response => {
-        console.log(response);
-    })
-    .catch(err => {
-        console.log("Deu erro!", err);
-    })
     }
+
+    onChangeName = (event) => {
+        this.setState({nameValue: event.target.value})
+    }
+
+    onChangeEmail = (event) => {
+        this.setState({emailValue: event.target.value})
+    }
+
+    onClickSalvarDados = () => {
+        console.log(this.state.nameValue);
+        console.log(this.state.emailValue)
+        const body = {
+            name: this.state.nameValue,
+            email: this.state.emailValue
+        }
+        axios.post(`${urlBase}/users`, body, {
+            headers: {
+                "api-token": "Ivana-Hamilton"
+            }
+        }).then(() => {
+            alert("Usuário criado com sucesso!");
+        }).catch(() => {
+            alert("Erro ao criar usuário!");
+        })
+    };   
     
     render(){
         return(
             <Container>
-                <label>Nome:</label>
+                <label htmlFor={"name"}>Nome:</label>
                 <input
                     type='text'
-                />
-
-                <br/><br/>
-
-                <label>E-mail:</label>
-                <input
-                    type='text'
+                    name={"name"}
+                    value={this.state.nameValue}
+                    onChange={this.onChangeName}
                 />
 
                 <br/>
 
-                <button>Salvar</button>
+                <label htmlFor={"email"}>E-mail:</label>
+                <input
+                    name={"email"}
+                    value={this.state.emailValue}
+                    onChange={this.onChangeEmail}
+                />
+
+                <br/>
+
+                <button onClick={this.onClickSalvarDados}>Salvar</button>
             </Container>
     
         )

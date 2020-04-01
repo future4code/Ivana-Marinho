ee3bcb158f37160d29e25cb9634f7b91a6125112
Ivana Batista Marinho <ivanammarinho@hotmail.com>
2020-04-01 17:20:10 -0300

    com botao ativo

--- semana7/semana7/future4users/future4users/src/App.js
+++ semana7/semana7/future4users/future4users/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import axios from 'axios';
 import Registro from './Components/Registro.js';
 import Lista from './Components/Lista.js';

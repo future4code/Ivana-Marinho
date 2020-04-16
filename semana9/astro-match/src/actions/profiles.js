35ee6b150b1da378e97e3a87dd1c6ca479194246
Ivana Batista Marinho <ivanammarinho@hotmail.com>
2020-04-16 18:04:26 -0300

    entrega parcial

---
+++ semana9/astro-match/src/actions/profiles.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const baseUrl = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/darvas"; //pegar a minha do postman
+
+export const clearSwipes = () => async (dispatch) => {
+	await axios.put(`${baseUrl}/clear`)
+}
+
+export const getProfile = () => async (dispatch) => {
+	const response = await axios.get(`${baseUrl}/person`)
+	console.log(response.data.profile)
+	dispatch(setProfile(responde.data.profile));
+}
+
+export const setProfile = (profile) => {
+	return {
+		type: "SET_PROFILE",
+		payload: {
+			profile,
+		}
+	}
+}

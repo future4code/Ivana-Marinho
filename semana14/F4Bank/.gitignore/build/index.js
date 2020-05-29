e8ce990502ee834478b82f4de58032ae89f68dbb
Ivana Batista Marinho <ivanammarinho@hotmail.com>
2020-05-29 17:01:19 -0300

    4Bank

--- semana14/F4Bank/.gitignore/build/index.js
+++ semana14/F4Bank/.gitignore/build/index.js
@@ -1,61 +1,73 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs = __importStar(require("fs"));
-const anoAtual = new Date().getFullYear();
-const contaDoUsuario = {
+class Conta {
+    constructor(saldo, cpf, nome) {
+        this.saldo = saldo;
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+    verificaSaldo() {
+        return this.saldo;
+    }
+    atualizaSaldo(deposito) {
+        if (deposito <= 0) {
+            throw new Error("O valor deve ser maior que zero!");
+        }
+        else {
+            return this.saldo += deposito;
+        }
+    }
+}
+const conta = new Conta(100, 1234, "Ivana");
+const novoSaldo = conta.atualizaSaldo(800);
+console.log(novoSaldo);
+/*
+type pessoa = {
+    cpf: number,
+    nome: string,
+    anoDeNascimento: number,
+    saldoInicial: number
+}
+
+const anoAtual: number = new Date().getFullYear();
+
+const contaDoUsuario: pessoa = {
     cpf: Number(process.argv[2]),
     nome: String(process.argv[3]),
     anoDeNascimento: Number(process.argv[4]),
     saldoInicial: 0
-};
-const arquivoDeContas = "todasAsContas.json";
-function criarConta(contaDoUsusario) {
-    const idadeDaPessoa = Math.abs(contaDoUsuario.anoDeNascimento - anoAtual);
-    if (idadeDaPessoa < 18) {
-        console.log("A conta não pode ser criada");
-    }
-    else {
-        try {
-            const lerArquivo = fs.readFileSync(arquivoDeContas);
-            const passaParaString = lerArquivo.toString();
-            const passaParaObjeto = JSON.parse(passaParaString);
-            if (passaParaObjeto.find((item) => item.cpf === contaDoUsuario.cpf)) {
-                console.log("Essa conta já foi criada!");
-            }
-            else {
-                passaParaObjeto.push(contaDoUsuario);
-                try {
-                    fs.writeFileSync("todasAsContas.json", JSON.stringify(passaParaObjeto));
-                }
-                catch (erro) {
-                    console.log("Tente de novo!");
-                }
-            }
-            ;
-        }
-        catch (erro) {
-            console.log("Tente mais uma vez!");
-        }
-    }
 }
-;
-criarConta(contaDoUsuario);
+
+const arquivoDeContas: string = "todasAsContas.json";
+
+function criarConta(contaDoUsusario:pessoa): void{
+    const idadeDaPessoa = Math.abs(contaDoUsuario.anoDeNascimento - anoAtual);
+    if(idadeDaPessoa < 18){
+        console.log("A conta não pode ser criada");
+    }else{
+
+        try{
+            const lerArquivo: Buffer = fs.readFileSync(arquivoDeContas);
+            const passaParaString: string = lerArquivo.toString();
+            const passaParaObjeto: any = JSON.parse(passaParaString);
+            if(passaParaObjeto.find((item:any) => item.cpf === contaDoUsuario.cpf)) {
+               
+                    console.log("Essa conta já foi criada!");
+                }else{
+                    passaParaObjeto.push(contaDoUsuario);
+                    
+                    try{
+                        fs.writeFileSync("todasAsContas.json", JSON.stringify(passaParaObjeto));
+                    }catch(erro){
+                        console.log("Tente de novo!");
+                    }
+                };
+
+        }catch(erro){
+            console.log("Tente mais uma vez!");
+
+        }
+    }
+};
+
+criarConta(contaDoUsuario);*/ 

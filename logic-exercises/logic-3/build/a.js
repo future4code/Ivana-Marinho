50d523c6890eb70df871570e392c661681e0b555
Ivana Batista Marinho <ivanammarinho@hotmail.com>
2020-07-08 14:20:44 -0300

    exercício 3

---
+++ logic-exercises/logic-3/build/a.js
@@ -0,0 +1,10 @@
+const palavra1 = "ivana";
+const palavra2 = "Ivana";
+function comparaStrings(palavra1, palavra2) {
+    if (palavra1 === palavra2) {
+        return true;
+    }
+    return false;
+}
+console.log(comparaStrings(palavra1, palavra2));
+//# sourceMappingURL=a.js.map
